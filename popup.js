2cfcd9e45ed8c3893d7f78ed9b44270f5277fe53
Julian Wagner <julian.wagner@procore.com>
2023-04-25 08:33:57 -0700

    refactored popup.js

--- popup.js
+++ popup.js
@@ -1,16 +1,4 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  var tab = tabs[0];
-  var url = tab.url;
-
-  var copyInput = document.createElement("input");
-  copyInput.setAttribute("type", "text");
-  copyInput.setAttribute("value", url);
-  document.body.appendChild(copyInput);
-  copyInput.select();
-  //document.execCommand("copy");
-  document.body.removeChild(copyInput);
-
-  var popupWindow = window.open("", "popup", "width=400,height=200");
-  popupWindow.document.write("<p>URL copied:</p><p><a href='" + url + "'>" + url + "</a></p>");
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+  var url = tabs[0].url;
+  document.getElementById('url').textContent = url;
 });
-
