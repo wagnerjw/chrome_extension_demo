129d7516e6038ca4fddd5fb6e1c8f3f9c3d76e68
Julian <julian@wagnerjw.dev>
2023-04-26 19:48:20 -0700

    refactor: changing popup.js logic

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-  var url = tabs[0].url;
-  document.getElementById('url').textContent = url;
+//Get current domain
+var domain = window.location.hostname;
+domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
+
+chrome.runtime.sendMessage({command: "fetch", data: {domain: domain}}, (response) => {
+  parseCoupons(response.data, domain);
 });
