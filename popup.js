192c3757ad671135255b4aed78f1f6affc0915a4
Julian <julian@wagnerjw.dev>
2023-04-26 21:06:35 -0700

    ref: popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 //Get current domain
 var domain = window.location.hostname;
-domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
 
-chrome.runtime.sendMessage({command: "fetch", data: {domain: domain}}, (response) => {
-  parseCoupons(response.data, domain);
-});
+
+//domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
+// chrome.runtime.sendMessage({command: "fetch", data: {domain: domain}}, (response) => {
+//   parseCoupons(response.data, domain);
+// });
+
