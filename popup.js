fc76694f62875e690e937c3cae48a806f871689d
Julian <julian@wagnerjw.dev>
2023-04-27 21:08:26 -0700

    feat: added regex url parser

--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
   var url = tabs[0].url;
   url = url.replace('http://', '').replace('https://', '').replace('www.','')
+  var splitURL = url.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
+  const fullPath = splitURL[5];
+  const rootPath = fullPath.split('/')[1];
   document.getElementById('url').href = url;
-  document.getElementById('url').textContent = url;
+  document.getElementById('url').textContent = rootPath;
 });
 
 // -----------------------------------------------------------------------------------------------------
