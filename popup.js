6c4740a3589b2a53918296b32b395d44136a1537
wagnerjw <julian@wagnerjw.dev>
2023-04-28 09:38:39 -0700

    feat: added console log for the full url path

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
   var url = tabs[0].url;
   var splitURL = url.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
   const fullPath = splitURL[5];
+  console.log(fullPath);
   const rootPath = fullPath.split('/')[1];
   const rootPathCaps = rootPath.charAt(0).toUpperCase() + rootPath.slice(1);
   document.getElementById('url').href = url;
