101f872acbec1ad75fa67880c800371591636e33
Julian <julian@wagnerjw.dev>
2023-04-27 21:18:16 -0700

    ref: fixed html page

--- popup.js
+++ popup.js
@@ -1,12 +1,31 @@
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
   var url = tabs[0].url;
-  url = url.replace('http://', '').replace('https://', '').replace('www.','')
   var splitURL = url.split(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/);
   const fullPath = splitURL[5];
   const rootPath = fullPath.split('/')[1];
   document.getElementById('url').href = url;
   document.getElementById('url').textContent = rootPath;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // -----------------------------------------------------------------------------------------------------
 // Explanation from ChatGPT:
